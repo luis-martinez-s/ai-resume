8eeecae413653a351a7a944ea9c8e4168d5fd146
Luis Martinez <martinez.s.luis@gmail.com>
2018-10-13 00:11:58 +0200

    second box updating correctly on mouse behavior

--- src/App.js
+++ src/App.js
@@ -4,12 +4,88 @@ import TopBar from './components/TopBar';
 import MenuBox from './components/MenuBox';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDefault: true,
+      isHover: false,
+      isFocus: false,
+      boxState: [{
+        isDefault: true,
+        isHover: false,
+        isFocus: false,
+        boxClass: 'menu-box-default',
+        displaymenuSideMoxes: 'none'
+      }],
+      boxClass: 'menu-box-default',
+      displaymenuSideMoxes: 'none'
+    };
+    this.onHover = this.onHover.bind(this);
+    this.onDefault = this.onDefault.bind(this);
+    // this.onFocus = this.onFocus.bind(this);
+    this.onHoverArr = this.onHoverArr.bind(this);
+    this.onDefaultArr = this.onDefaultArr.bind(this);
+    this.onFocusArr = this.onFocusArr.bind(this);
+  }
+  onDefault() {
+    if (!this.state.isFocus) {
+      this.setState({
+        boxClass: 'menu-box-default',
+        isDefault: true
+      });
+    }
+  }
+  onHover() {
+    if (!this.state.isFocus) {
+      this.setState({
+        boxClass: 'menu-box-hover',
+        isHover: true
+      });
+    }
+  }
+  // onFocus() {
+  //   this.setState({
+  //     boxClass: 'menu-box-focus',
+  //     displaymenuSideMoxes: 'initial',
+  //     isFocus: true
+  //   });
+  // }
+  onDefaultArr() {
+    if (!this.state.isFocus) {
+      this.setState({
+        boxClass: 'menu-box-default',
+        isDefault: true
+      });
+    }
+  }
+  onHoverArr() {
+    if (!this.state.isFocus) {
+      this.setState({
+        boxClass: 'menu-box-hover',
+        isHover: true
+      });
+    }
+  }
+  onFocusArr() {
+    this.state.boxState[0].boxClass = 'menu-box-focus';
+    this.state.boxState[0].displaymenuSideMoxes = 'initial'
+    this.state.boxState[0].isFocus = true
+    this.forceUpdate();
+  }
   render() {
     return (
       <div className="App">
         <div id='main-container'>
           <TopBar />
-          <MenuBox />
+          <div id='menu-box-container'>
+            <div id='container-header'><span className='cpu-text' id='folder-header'>Folders</span></div>
+            <MenuBox boxClass={this.state.boxClass} sideBoxes={this.state.displaymenuSideMoxes} hoverHandle={this.onHover} defaultHandle={this.onDefault} focusHandle={this.onFocus} />
+            <MenuBox boxClass={this.state.boxState[0].boxClass} sideBoxes={this.state.boxState[0].displaymenuSideMoxes} hoverHandle={this.onHover} defaultHandle={this.onDefault} focusHandle={this.onFocusArr} />
+            <MenuBox boxClass={this.state.boxClass} sideBoxes={this.state.displaymenuSideMoxes} hoverHandle={this.onHover} defaultHandle={this.onDefault} focusHandle={this.onFocus} />
+            <MenuBox boxClass={this.state.boxClass} sideBoxes={this.state.displaymenuSideMoxes} hoverHandle={this.onHover} defaultHandle={this.onDefault} focusHandle={this.onFocus} />
+            <MenuBox boxClass={this.state.boxClass} sideBoxes={this.state.displaymenuSideMoxes} hoverHandle={this.onHover} defaultHandle={this.onDefault} focusHandle={this.onFocus} />
+            <MenuBox boxClass={this.state.boxClass} sideBoxes={this.state.displaymenuSideMoxes} hoverHandle={this.onHover} defaultHandle={this.onDefault} focusHandle={this.onFocus} />
+          </div>
         </div>
       </div>
     );
--- src/components/MenuBox.js
+++ src/components/MenuBox.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
 class MenuBox extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
         return (
-            <div className='menu-box green-border'><span className='cpu-text menu-box-text'>education</span></div>
+            <div className='box-wrapper'>
+                <div className={this.props.boxClass} onMouseEnter={this.props.hoverHandle} onMouseLeave={this.props.defaultHandle} onClick={this.props.focusHandle}>
+                    <div className='menu-box-before' style={{ display: this.props.sideBoxes }}></div>
+                    <span className='cpu-text menu-box-text'>education</span>
+                    <div className='menu-box-after' style={{ display: this.props.sideBoxes }}></div>
+                </div>
+            </div>
         );
     }
 }
