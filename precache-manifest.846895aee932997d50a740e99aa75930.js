a17ac939829d114b1982bdd5ae3c0696489f69c6
Luis Martinez <martinez.s.luis@gmail.com>
2018-10-14 20:51:36 +0200

    Updates

---
+++ precache-manifest.846895aee932997d50a740e99aa75930.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "88a9ffb8150e4541a6e7ad1dd686a16b",
+    "url": "/ai-resume/static/media/hellovetica.88a9ffb8.ttf"
+  },
+  {
+    "revision": "48867061987b540a3f585de31934b67f",
+    "url": "/ai-resume/static/media/hellovetica-webfont.48867061.woff"
+  },
+  {
+    "revision": "0fe620076652d56db2aaa1c1560c9ad2",
+    "url": "/ai-resume/static/media/hellovetica-webfont.0fe62007.woff2"
+  },
+  {
+    "revision": "16b4b35828bbea101a8e",
+    "url": "/ai-resume/static/js/runtime~main.16b4b358.js"
+  },
+  {
+    "revision": "4359fe617fa8397154e4",
+    "url": "/ai-resume/static/js/main.4359fe61.chunk.js"
+  },
+  {
+    "revision": "e1ada48e35de0e3b8be4",
+    "url": "/ai-resume/static/js/1.e1ada48e.chunk.js"
+  },
+  {
+    "revision": "4359fe617fa8397154e4",
+    "url": "/ai-resume/static/css/main.a319ae4c.chunk.css"
+  },
+  {
+    "revision": "6b978da531b2df60983905816c9ab3a5",
+    "url": "/ai-resume/index.html"
+  }
+];
